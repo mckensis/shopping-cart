850b9bd53206d306a17799b483c0c4b401a872e4
Aidan Mckenzie <adn.mck@gmail.com>
2023-05-02 18:30:12 +0100

    updated checkout styles

--- src/Checkout.js
+++ src/Checkout.js
@@ -7,30 +7,32 @@ const Checkout = () => {
 
   return (
     <main>
-      <section className="order-summary">
-        <h2>Order Summary</h2>
-        {cart.length > 0 && <>
-        {cart.map((item) => (
-          <article>
-            <h3>{item.product.common} x{item.quantity}</h3>
-            <p>£{item.product.price}</p>
-          </article>
-        ))}
-        <hr />
-        <section className="totals">
-          <p>Subtotal <span>£{returnCartCost()}</span></p>
-          <p>Delivery <span>£{returnDeliveryCost()}</span></p>
-          <p className="total">Total <span>£{returnTotalCost()}</span></p>
-        </section>
-        <hr />
-        <Link className="big-link" to="/checkout">Confirm Order</Link>
-        </>}
-        {cart.length === 0 &&
-          <section className="empty">
-            <h4>Your shopping cart is empty!</h4>
-            <Link className="big-link" to="/shop">Return to shop</Link>
+      <section className="container">
+        <section className="order-summary">
+          <h2>Order Summary</h2>
+          {cart.length > 0 && <>
+          {cart.map((item) => (
+            <article>
+              <h3>{item.product.common} x{item.quantity}</h3>
+              <p>£{item.product.price}</p>
+            </article>
+          ))}
+          <hr />
+          <section className="totals">
+            <p>Subtotal <span>£{returnCartCost()}</span></p>
+            <p>Delivery <span>£{returnDeliveryCost()}</span></p>
+            <p className="total">Total <span>£{returnTotalCost()}</span></p>
           </section>
-        }
+          <hr />
+          <Link className="big-link" to="/checkout">Confirm Order</Link>
+          </>}
+          {cart.length === 0 &&
+            <section className="empty">
+              <h4>Your shopping cart is empty!</h4>
+              <Link className="big-link" to="/shop">Return to shop</Link>
+            </section>
+          }
+        </section>
       </section>  
     </main>
   )
