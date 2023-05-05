b918f9ea9535b8e2b435159607e43cf55ce2ef52
Aidan Mckenzie <adn.mck@gmail.com>
2023-05-05 19:05:36 +0100

    homepage testing with react testing library

--- src/testing/Homepage.test.js
+++ src/testing/Homepage.test.js
@@ -3,14 +3,27 @@ import { render, screen } from "@testing-library/react";
 import Homepage from '../components/Homepage';
 import { HashRouter } from "react-router-dom";
 
-// Include HashRouter in the render due to the Link components that exist in Homepage
-describe("App component", () => {
+// Test that Homepage renders
+describe("Homepage component", () => {
   it("renders correct heading", () => {
     render(
+      // Include HashRouter in the render due to the Link components that exist in Homepage
       <HashRouter basename="/">
         <Homepage />
       </HashRouter>
       );
     expect(screen.getByRole("heading").textContent).toMatch(/welcome to planty/i);
+  });
+});
+
+// Test that the link to the shop renders
+describe("Homepage component shop link", () => {
+  it("renders a link to the shop", () => {
+    render(
+      <HashRouter basename="/">
+        <Homepage />
+      </HashRouter>
+    );
+    expect(screen.getByRole("link").textContent).toMatch(/shop now/i);
   });
 });
